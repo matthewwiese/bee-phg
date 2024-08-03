27f2c9f6e7872a98b25255541776128171bfd651
Matt Wiese <matthewwiese@users.noreply.github.com>
2024-08-03 16:16:50 -0400

    Use assembly file list instead

--- scripts/build-align.sh
+++ scripts/build-align.sh
@@ -5,11 +5,8 @@
 
 # Align assemblies
 mkdir -p data/align
-phg align-assemblies                                                             \
-    --gff data/reference/genomic.gff                                             \
-    --reference-file data/reference/GCF_003254395.2_Amel_HAv3.1_genomic.fna      \
-    --assembly-file data/assemblies/GCA_019321825.1_ASM1932182v1_genomic.fna     \
-    --assembly-file data/assemblies/GCA_013841245.2_ASM1384124v2_genomic.fna     \
-    --assembly-file data/assemblies/GCA_003314205.2_INRA_AMelMel_1.1_genomic.fna \
-    --assembly-file data/assemblies/GCF_000002195.4_Amel_4.5_genomic.fna         \
+phg align-assemblies                                                        \
+    --gff data/reference/genomic.gff                                        \
+    --reference-file data/reference/GCF_003254395.2_Amel_HAv3.1_genomic.fna \
+    --assembly-file-list assemblies.txt                                     \
     -o data/align
